63db4e43236f31ba3a7aae6ea3d0679d9106c35a
Bayarmaa Bumandorj <141606995+bayarmaa01@users.noreply.github.com>
2026-02-16 11:34:12 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello SonarQube!");
+    }
+
+    public String message() {
+        return "Hello SonarQube!";
+    }
+}
